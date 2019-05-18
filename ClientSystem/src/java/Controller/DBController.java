8eea217fdebaa5fed33916cbed20b77e0cf0c59f
Sylphy0052 <yark0531@yahoo.co.jp>
2019-05-19 02:00:24 +0900

    feat: manipulate staff table

--- ClientSystem/src/java/Controller/DBController.java
+++ ClientSystem/src/java/Controller/DBController.java
@@ -2,10 +2,12 @@ package Controller;
 
 import DBModel.ClientTB;
 import DBModel.CourseTB;
+import DBModel.StaffTB;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 @Stateless
 public class DBController {
@@ -22,12 +24,35 @@ public class DBController {
         em.persist(course);
     }
     
+    public void add(StaffTB staff) {
+        em.persist(staff);
+    }
+    
+    public StaffTB getStaff(String name, String pass) {
+        TypedQuery<StaffTB> query = em.createQuery(
+                "SELECT s FROM StaffTB s WHERE s.name = :name AND s.pass = :pass",
+                StaffTB.class
+        );
+        return query
+                .setParameter("name", name)
+                .setParameter("pass", pass)
+                .getSingleResult();
+    }
+    
     public List<ClientTB> getClients() {
         return em.createQuery("SELECT c FROM ClientTB c").getResultList();
     }
     
     public List<CourseTB> getCourseList() {
         return em.createQuery("SELECT c FROM CourseTB c").getResultList();
+    }
+    
+    public List<StaffTB> getStaffList() {
+        return em.createQuery("SELECT s FROM StaffTB s").getResultList();
+    }
+    
+    public void delete(StaffTB staff) {
+        em.remove(em.merge(staff));
     }
     
 }
