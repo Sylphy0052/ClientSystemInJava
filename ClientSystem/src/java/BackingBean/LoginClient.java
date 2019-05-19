1176d7797daeae107946ad6a9f1184b5a3846b8e
Sylphy0052 <yark0531@yahoo.co.jp>
2019-05-20 02:09:52 +0900

    add: login client backing bean

--- ClientSystem/src/java/BackingBean/LoginClient.java
+++ ClientSystem/src/java/BackingBean/LoginClient.java
@@ -1,14 +1,92 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package BackingBean;
 
-/**
- *
- * @author pyente
- */
-public class LoginClient {
+import Controller.DBController;
+import DBModel.ClientTB;
+import Utils.Utils;
+import java.io.Serializable;
+import java.util.Date;
+import javax.ejb.EJB;
+import javax.ejb.EJBException;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@SessionScoped
+@Named
+public class LoginClient implements Serializable {
+    private String name;
+    private String pass;
     
+    private ClientTB client;
+    
+    private String message = "";
+    
+    @EJB
+    DBController dbc;
+    
+    public LoginClient(){}
+    
+    public String login() {
+        try {
+            if(checkLogin()) {
+                clear();
+            }
+            return "mypage.xhtml";
+        }catch(EJBException e) {
+            clear();
+            message = "Invalid name or password";
+            return null;
+        }
+    }
+    
+    public void clear() {
+        name = "";
+        pass = "";
+        message = "";
+    }
+    
+    private boolean checkLogin() {
+        this.client = dbc.getClient(name, pass);
+        return this.client != null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public ClientTB getClient() {
+        return client;
+    }
+
+    public void setClient(ClientTB client) {
+        this.client = client;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+    public String getAttendDate() {
+        Date d = this.client.getAttendDate().get(0).getAttendDate();
+        return Utils.convertDateFromString(d);
+    }
+    
+    public String getCourse() {
+        return this.client.getCourse().getName();
+    }
 }
