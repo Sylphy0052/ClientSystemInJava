e17b81d76891578724a68e22658d606422d41071
Sylphy0052 <yark0531@yahoo.co.jp>
2019-05-20 00:22:04 +0900

    feat: date compare function

--- ClientSystem/src/java/Utils/Utils.java
+++ ClientSystem/src/java/Utils/Utils.java
@@ -2,7 +2,9 @@ package Utils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Utils {
     public static Date convertDate(String date) {
@@ -15,5 +17,25 @@ public class Utils {
     
     public static String convertDateFromString(Date d) {
         return new SimpleDateFormat("yyyy-MM-dd").format(d);
+    }
+    
+    public static int compareDate(Date d1, Date d2) {
+        Date today = truncate(d1);
+        Date target = truncate(d2);
+        if(today.equals(target)) {
+            return 0;
+        } else if(today.before(target)) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+    
+    public static Date truncate(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return new GregorianCalendar(
+                cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DATE)
+        ).getTime();
     }
 }
